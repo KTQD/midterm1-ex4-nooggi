a17fb6d2d34b27c49b4d5fa41185d4f6855ab75c
nooggi <160413823+nooggi@users.noreply.github.com>
2024-03-16 14:42:29 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,33 @@
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
-    
 
+        // luôồng 1 tính tổng cacs phần tử
+        Thread ThreadTinhTong = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int sum = 0;
+                for (int num : numbers) {
+                    sum += num;
+                }
+                System.out.println("Tổng của các phần tử trong mảng là: " + sum);
+            }
+        });
+        ThreadTinhTong.start();
 
-        Viết mã của bạn vào đây
-    
+        // luồng 2 tìm và in ra phần tử lớn nhất trong mảng
+        Thread Threadmax = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int max = numbers[0];
+                for (int i = 1; i < numbers.length; i++) {
+                    if (numbers[i] > max) {
+                        max = numbers[i];
+                    }
+                }
+                System.out.println("Phần tử lớn nhất trong mảng là: " + max);
+            }
+        });
+        Threadmax.start();
     }
 }
